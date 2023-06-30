206ec1b3e1c72a09d29a14a3390ac703cfc19d45
christian1913 <75808879+christian1913@users.noreply.github.com>
2023-06-30 02:55:52 +0200

    .

--- backend/correos/urls.py
+++ backend/correos/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     # Paginas
 
     path('grupos/<str:id>', views.index, name='correos'),
-    path('enviar-archivo/<str:id>', views.enviar_archivo, name='enviar_archivo'),
 
     
 ]
